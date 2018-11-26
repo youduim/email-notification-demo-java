71b553358f6ed1a48f85d3cad781f5ca2a2d35c7
fengqingsadan <fengqingsadan@126.com>
2018-11-26 10:59:34 +0800

    邮件消息逻辑调整

--- src/main/java/im/youdu/sdk/demo/exmail/ExmailMsgServlet.java
+++ src/main/java/im/youdu/sdk/demo/exmail/ExmailMsgServlet.java
@@ -1,6 +1,7 @@
 package im.youdu.sdk.demo.exmail;
 
 import im.youdu.sdk.client.AppClient;
+import im.youdu.sdk.client.MailMsgClient;
 import im.youdu.sdk.entity.Const;
 import im.youdu.sdk.entity.EmailBody;
 import im.youdu.sdk.entity.YDApp;
@@ -13,6 +14,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.Calendar;
 import java.util.Properties;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -23,7 +25,7 @@ public class ExmailMsgServlet  extends HttpServlet {
     private ExecutorService threadPool = Executors.newCachedThreadPool();
 
     private String initErrMsg = "";
-    private AppClient ydAppClient;
+    private MailMsgClient mailMsgClient;
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -33,7 +35,7 @@ public class ExmailMsgServlet  extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String respMsg = "ok";
-        if(null == ydAppClient){
+        if(null == mailMsgClient){
                 log.error("有度服务信息未初始化:"+initErrMsg);
                 return;
         }
@@ -59,72 +61,82 @@ public class ExmailMsgServlet  extends HttpServlet {
             String buinStr = prop.getProperty("buin");
             int buin = Integer.valueOf(buinStr);
             String host = prop.getProperty("host");
-            String appId = prop.getProperty("appId");
-            String aesKey = prop.getProperty("appAESKey");
-            String fmtStr = String.format("{buin:%d; host:%s; appId:%s; appAESKey: %s}", buin, host, appId, aesKey);
+            String exmailAppAESKey = prop.getProperty("exmailAppAESKey");
+            String fmtStr = String.format("{buin:%d; host:%s; appAESKey: %s}", buin, host, exmailAppAESKey);
             log.info("读取到有度企业邮应用配置:"+fmtStr);
-
-            YDApp ydApp = new YDApp();
-            ydApp.setBuin(buin);
-            ydApp.setHost(host);
-            ydApp.setAppId(appId);
-            ydApp.setAppAesKey(aesKey);
-            ydAppClient = new AppClient(ydApp);
+            mailMsgClient = new MailMsgClient(buin, host, exmailAppAESKey);
         } catch (Exception e) {
             initErrMsg = "读取有度企业邮应用配置发生错误: "+e.getMessage();
             log.error(initErrMsg);
         }
     }
 
-    //TODO 这里只是一段示例代码, 具体实现请参照您企业邮箱服务商的接口规范
-    private void parseRequest(HttpServletRequest req) throws Exception{
+    private void parseRequest(HttpServletRequest req){
+        String toUser = "test1";
+//        String toEmail = "test1@test.com";
+        EmailBody email = new EmailBody();
+        email.setFromUser("test2");
+//        email.setFromEmail("test2@test.com");
+        email.setSubject("测试邮件消息");
+        email.setAction(Const.Mail_Msg_New);
+        email.setTimex(Calendar.getInstance().getTimeInMillis());
+        MailTread thread = new MailTread(toUser, "", email);
+        threadPool.execute(thread);
+    };
+
+    //TODO 这里是一段示例代码, 具体实现请参照您企业邮箱服务商的接口规范
+    private void parseRequest_demo(HttpServletRequest req) throws Exception{
         String msgType = req.getParameter("msgType");
         if(null == msgType){
             throw new Exception("消息类型为空");
         }
+        String toUser = req.getParameter("toUser");
 
         EmailBody email = new EmailBody();
         long sendTime = 0;
         String timeStr = req.getParameter("time");
         if(null != timeStr){
             sendTime = Long.valueOf(timeStr);
+        }else{
+            sendTime = Calendar.getInstance().getTimeInMillis();
         }
-        String toUser = req.getParameter("toUser");
         email.setTimex(sendTime);
-        email.setToUser(toUser);
-
         if("newMail".equals(msgType)){
-            String title = req.getParameter("title");
+            String subject = req.getParameter("subject");
             String fromUser = req.getParameter("fromUser");
-            String mailUrl = req.getParameter("mailUrl");
+            String mailLink = req.getParameter("mailLink");
 
-            email.setAction(Const.Email_Action_NewMail);
-            email.setTitle(title);
+            email.setAction(Const.Mail_Msg_New);
+            email.setSubject(subject);
             email.setFromUser(fromUser);
-            email.setMailUrl(mailUrl);
+            email.setLink(mailLink);
         }else if("unread" .equals(msgType)){
             String unReadStr = req.getParameter("unReadCount");
             int unReadCount = Integer.valueOf(unReadStr);
-            email.setAction(Const.Email_Action_UnRead);
-            email.setUnread(unReadCount);
+            email.setAction(Const.Mail_Msg_Unread);
+            email.setUnreadCount(unReadCount);
         }else{
             throw new Exception("位置的消息类型："+msgType);
         }
-        MailTread thread = new MailTread(email);
+        MailTread thread = new MailTread(toUser, "", email);
         threadPool.execute(thread);
     }
 
     class MailTread implements Runnable{
-         EmailBody email;
+        String toUser;
+        String toEmail;
+        EmailBody email;
 
-        public MailTread(EmailBody _email){
+        public MailTread(String _toUser, String _toEmail, EmailBody _email){
+            this.toUser = _toUser;
+            this.toEmail = _toEmail;
             this.email = _email;
         }
 
         @Override
         public void run() {
             try {
-                ydAppClient.sendMailMsg (this.email);
+                mailMsgClient.sendMailMsg (toUser,toEmail,this.email);
             } catch (Exception e) {
                 log.error("推送新邮件到有度异常："+e.getMessage());
             }
